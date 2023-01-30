8345a9bd14432ecd2b042ec95542bd276e7e9a64
Rada Berar <ujagaga@gmail.com>
2023-01-30 16:01:39 +0100

    Updated crud

--- sql_app/crud.py
+++ sql_app/crud.py
@@ -12,8 +12,18 @@ def get_user_by_token(db: Session, token: str):
     return db.query(models.User).filter(models.User.token == token).first()
 
 
+def create_local_user(db: Session):
+    user = models.User(username=config.LOCAL_USER_NAME, hashed_password="local_pass123", email="local@officeserver.local")
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
+
+
 def update_user_token(db: Session, username: str, token: str):
     user = db.query(models.User).filter(models.User.username == username).first()
+    if username == config.LOCAL_USER_NAME and user is None:
+        user = create_local_user()
     user.token = token
     user.token_expire_time = time.time() + config.TOKEN_DURATION
     db.commit()
